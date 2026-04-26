0c1d7e1e385e7a47119a6278b5737d4b1669b4b0
Sajit Baral <baralsajit@gmail.com>
2026-04-26 21:34:37 +0545

    Custom User registration done

--- simpleproject/simpleproject/settings.py
+++ simpleproject/simpleproject/settings.py
@@ -124,3 +124,10 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_URL = '/accounts/login/'
+
+LOGIN_REDIRECT_URL = '/tweet/'
+
+LOGOUT_REDIRECT_URL = '/tweet/'
+
--- simpleproject/tweet/views.py
+++ simpleproject/tweet/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Tweet
-from .forms import TweetForm
+from .forms import TweetForm, UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 
 def index(request):
     return render(request, 'index.html')
@@ -9,6 +11,7 @@ def tweet_list(request):
     tweets= Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets':tweets})
 
+@login_required
 def create_tweet(request):
     if request.method == 'POST':
         form= TweetForm(request.POST, request.FILES)
@@ -23,6 +26,7 @@ def create_tweet(request):
 
     return render(request, 'create_tweet.html', {'form':form})
 
+@login_required
 def edit_tweet(request, tweet_id):
     tweet= get_object_or_404(Tweet, pk=tweet_id, user= request.user)    #here  by adding user=request.user, we prevent a "Hacker" from changing someone else's tweet just by guessing the ID number
     if request.method=='POST':
@@ -38,6 +42,7 @@ def edit_tweet(request, tweet_id):
 
     return render(request, 'create_tweet.html', {'form':form})
 
+@login_required
 def delete_tweet(request, tweet_id):
     tweet= get_object_or_404(Tweet, pk=tweet_id, user=request.user)
     if request.method=='POST':
@@ -45,3 +50,19 @@ def delete_tweet(request, tweet_id):
         return redirect('tweet_list')
     
     return render(request, 'delete_tweet.html', {'tweet':tweet})
+
+def register(request):
+    if request.method=='POST':
+        form= UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user= form.save(commit= False)
+            user.set_password(form.cleaned_data['password1'])
+            form.save()
+            login(request, user)
+            return redirect('tweet_list')
+    
+    else:
+        form= UserRegistrationForm()
+
+    return render(request, 'registration/register.html', {'form':form})
+    
