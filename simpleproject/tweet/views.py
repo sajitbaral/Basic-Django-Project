0bac24985d358aa8490ae1cc847039ebb879aed6
Sajit Baral <sajit.baral.cloud@gmail.com>
2026-08-31 23:04:09 +0545

    Slight file changes

--- simpleproject/tweet/views.py
+++ simpleproject/tweet/views.py
@@ -32,7 +32,7 @@ def edit_tweet(request, tweet_id):
     if request.method=='POST':
         form= TweetForm(request.POST, request.FILES, instance=tweet)    #instance=tweet updates the existing value of tweet in db. doesnot makes duplicate. It tells Django, "Don't make a new tweet; just apply these changes to the specific tweet we found in line 2
         if form.is_valid():
-            tweet=form.save(commit=False)
+            tweet=form.save(commit = False)
             tweet.user= request.user
             tweet.save()
             return redirect('tweet_list')
